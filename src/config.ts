dbb23eb2a0d09e190a8d51958a38aa4bc5f26df8
Another_Dayu <dayuguo1129@outlook.com>
2023-04-07 14:07:16 +0800

    test

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Captain Awesome";
+export const SITE_TITLE = "One Health  Notebook";
 export const SITE_DESCRIPTION =
-  "Welcome to my blog! I write about modern web development.";
+  "大家好，这是一个分享跨学科研究资料，培养批判性、系统性思考能力的社区。";
 export const TWITTER_HANDLE = "@yourtwitterhandle";
 export const MY_NAME = "Captain Awesome";
 
